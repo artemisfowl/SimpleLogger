ee9408683ed44e8ca273f457b424b2d5f5cf20e8
artemisfowl <skb655952@gmail.com>
2021-12-20 13:01:47 +0530

    Added Console Logging example

---
+++ src/dev/sb/examples/ConsoleLogging.java
@@ -0,0 +1,37 @@
+package dev.sb.examples;
+
+import static java.lang.System.out;
+
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import dev.sb.SimpleLogger.SimpleLogger;
+
+/**
+ * @file ConsoleLogging.java
+ * @author sb
+ * @brief Example show-casing simple console logging capacity
+ */
+
+public class ConsoleLogging {
+	/* set up the logger and then call the functions */
+	private SimpleLogger simpleLogger = new SimpleLogger(); 
+	private Logger logger = null;
+	
+	public void executeLoggerFunctions() {
+		try {
+			simpleLogger.setupLogger(null, "logs", "log_n", null, true, true, Level.FINEST);
+		} catch (IOException ioException) {
+			ioException.printStackTrace();
+		} catch (Exception exception) {
+			exception.printStackTrace();
+		}
+
+		out.println(SimpleLogger.getLoggingFormat());
+		out.println(simpleLogger.getLogFilepath());
+		
+		logger = SimpleLogger.getLogger();
+		logger.warning("Testing a write");
+	}
+}
